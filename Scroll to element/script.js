4be4ebb306adc9f7951ece3d52371fed8d1d0607
unknown <magurawork@gmail.com>
2018-12-29 14:44:34 +0300

    add scroll to element script

---
+++ Scroll to element/script.js
@@ -0,0 +1,12 @@
+$(function() {
+
+  $(".menu .menu-item").on("click", "a", function(event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({
+      scrollTop: top - 15
+    }, 1000);
+  });
+
+});
